64de48e754cc9c6539d6141e9bdca74049f56e18
amoghmaddur <maddur.a@northeastern.edu>
2025-10-26 23:10:57 -0400

    Admin role created

--- Group_Assn_Team_3/src/Business/Profiles/EmployeeProfile.java
+++ Group_Assn_Team_3/src/Business/Profiles/EmployeeProfile.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Business.Profiles;
+
+import Business.Person.Person;
 
 /**
  *
  * @author amoghvenkat
  */
-public class EmployeeProfile {
-    
+public class EmployeeProfile extends Profile {
+
+
+
+    public EmployeeProfile(Person p) {
+
+        super(p); 
+
+    }
+    @Override
+    public String getRole(){
+        return  "Admin";
+    }
+
 }
